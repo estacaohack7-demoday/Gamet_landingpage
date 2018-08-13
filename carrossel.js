d4c4835601637891d0c90bfa88e157b898a85e39
mautsunomiya <mari_utsu@hotmail.com>
2018-08-13 18:55:11 -0300

    Parte carrossel da landingpage sem JS

---
+++ carrossel.js
@@ -0,0 +1,30 @@
+let setas = document.queryCommandValue(".seta");
+let seta_direita = document.queryCommandValue("#direita");
+let seta_esquerda = document.queryCommandValue("#esquerda");
+let quadros = document.queryCommandValue(".quadro");
+let quadro_um = document.queryCommandValue("#um");
+let quadro_dois = document.queryCommandValue("#dois");
+let quadro_tres = document.queryCommandValue("#tres");
+let quadro_quatro = document.queryCommandValue("#quatro");
+let quadro_cinco = document.queryCommandValue("#cinco");
+let pin = document.queryCommandValue(".pin");
+let pin_primeiro = document.queryCommandValue("#primeiro");
+let pin_segundo = document.queryCommandValue("#segundo");
+let pin_terceiro = document.queryCommandValue("#terceiro");
+let pin_quarto = document.queryCommandValue("#quarto");
+let pin_quinto = document.queryCommandValue("#quinto");
+
+
+let pin = [pin_primeiro, pin_segundo, pin_terceiro, pin_quarto, pin_quinto]
+
+for (i=0, i<= carrosel.length, i++){;
+onclick(carrosel){
+    i+2 && i+3 = style.display="none";
+    style.transforme="translateX(100%)"
+}
+
+for pin of carrosel{}
+
+function girarCarrossel (quadros){
+    
+}
